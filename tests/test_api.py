bb47ebdeec6ef87aebb7ff150c1b5e2f9d29f5da
MarcosCantelli <marcoscantelli@hotmail.com>
2026-04-25 16:37:43 -0300

    teste de ticket

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,13 +7,3 @@ def test_health():
     assert response.status_code == 200
     assert response.json["status"] == "ok"
     
-def test_create_ticket():
-    client = app.test_client()
-
-    response = client.post("/ticket", json={
-        "user": "teste",
-        "description": "teste pipeline"
-    })
-
-    assert response.status_code == 200
-    assert "id" in response.json
