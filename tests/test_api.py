190785a56638e634bb5bf6b664003fd33d7b7171
MarcosCantelli <marcoscantelli@hotmail.com>
2026-04-25 16:49:29 -0300

    quality gate

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,11 @@
 from app.api.app import app
 
-def test_health():
+def test_create_ticket():
     client = app.test_client()
-    response = client.get("/")
+
+    response = client.post("/ticket", json={
+        "user": "teste",
+        "description": "teste de chamado"
+    })
 
     assert response.status_code == 200
-    assert response.json["status"] == "ok"
-    
