ebdd7410109e7d7f83a23318dbe47352c9863817
MarcosCantelli <marcoscantelli@hotmail.com>
2026-04-25 15:58:34 -0300

    correção para execção em ARM e melhorias no bot

--- app/database/db.py
+++ app/database/db.py
@@ -13,7 +13,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
--- app/models/ticket.py
+++ app/models/ticket.py
@@ -1,10 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from app.database.db import Base
 
+
 class Ticket(Base):
     __tablename__ = "tickets"
 
     id = Column(Integer, primary_key=True, index=True)
     user = Column(String)
+    category = Column(String)       # hardware / software
+    subcategory = Column(String)    # impressora / 3d
     description = Column(String)
+    ai_suggestion = Column(String)
     status = Column(String, default="aberto")
